2f48292f12304385a07a36187830601272bcc380
MiguelEmmara-ai <m.miguelemmara@gmail.com>
2022-11-25 01:30:40 +1300

    Table Products

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +15,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [DashboardController::class, 'index']);
+Route::get('/', [DashboardController::class, 'index'])
+     ->name('dashboard');
 
-Auth::routes(['register' =>false]);
+Auth::routes(['register' => false]);
+
+Route::resource('products', ProductController::class);
