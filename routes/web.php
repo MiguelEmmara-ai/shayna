1b17e85ad80cd3b2643fa0f5171c791456c8fa07
MiguelEmmara-ai <m.miguelemmara@gmail.com>
2022-11-25 21:15:51 +1300

    Transaksi -> Model, Index

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\ProductGalleryController;
+use App\Http\Controllers\TransactionController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -23,7 +24,8 @@ Auth::routes(['register' => false]);
 
 Route::get('products/{id}/gallery', [ProductController::class, 'gallery'])
      ->name('products.gallery');
-
 Route::resource('products', ProductController::class);
 
 Route::resource('product-galleries', ProductGalleryController::class);
+
+Route::resource('transactions', TransactionController::class);
